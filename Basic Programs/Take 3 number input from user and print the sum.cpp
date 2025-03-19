4f471989f4d7ec9b5c9c726d87823d047fe5cc73
Shampy <157746114+codershampy@users.noreply.github.com>
2025-03-19 22:50:52 +0530

    Add files via upload

---
+++ Basic Programs/Take 3 number input from user and print the sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout <<"Enter 1st number:";
+    cin >> x;
+    int y;
+    cout <<"Enter 2nd number:";
+    cin >> y;
+    int z;
+    cout <<"Enter 3rd number:";
+    cin >> z;
+    cout << "Sum is:" << x + y + z;
+}
